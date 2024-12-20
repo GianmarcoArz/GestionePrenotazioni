d0dc6b1f00dd3a0ced7c5b9c4a41a4af471ed4d4
Arzanese Gianmarco <gianmarcoarzanese@gmail.com>
2024-12-20 16:07:44 +0100

    Commit Finale

--- src/main/java/it/epicode/GestionePrenotazioni/MainRunner.java
+++ src/main/java/it/epicode/GestionePrenotazioni/MainRunner.java
@@ -47,77 +47,77 @@ public class MainRunner implements CommandLineRunner {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            // Mostra l'elenco degli utenti
-            List<Utente> utenti = utenteRepository.findAll();
-            System.out.println("Seleziona un utente:");
-            for (int i = 0; i < utenti.size(); i++) {
-                System.out.println((i + 1) + ". " + utenti.get(i).getNomeCompleto());
-            }
-            int utenteIndex = scanner.nextInt() - 1;
-            Utente utente = utenti.get(utenteIndex);
-
-            // Mostra l'elenco degli edifici
-            List<Edificio> edifici = edificioRepository.findAll();
-            System.out.println("Seleziona un edificio:");
-            for (int i = 0; i < edifici.size(); i++) {
-                System.out.println((i + 1) + ". " + edifici.get(i).getNome() + " - " + edifici.get(i).getCitta());
-            }
-            int edificioIndex = scanner.nextInt() - 1;
-            Edificio edificio = edifici.get(edificioIndex);
+            Utente utente = selectUtente(scanner);
+            Edificio edificio = selectEdificio(scanner);
 
             while (true) {
-                // Mostra la tabella delle prenotazioni
-                List<Prenotazione> prenotazioni = prenotazioniRepository.findAll();
-                System.out.println("Prenotazioni presenti nel nostro calendario:");
-                System.out.printf("%-15s %-20s %-20s %-20s%n", "Data", "Tipo Postazione", "Utente", "Città");
-                for (Prenotazione prenotazione : prenotazioni) {
-                    System.out.printf("%-15s %-20s %-20s %-20s%n", prenotazione.getDataPrenotazione(), prenotazione.getPostazione().getTipo(), prenotazione.getUtente().getNomeCompleto(), prenotazione.getPostazione().getEdificio().getCitta());
-                }
+                mostraPrenotazioni();
 
-                // Chiedi all'utente se desidera procedere con una prenotazione o vedere le prenotazioni per tipo e città
                 System.out.println("Desidera procedere con una prenotazione o vedere tutte le prenotazioni di un tipoPostazione di una determinata città? (prenotazione/vedere)");
                 String scelta = scanner.next();
 
                 if (scelta.equalsIgnoreCase("vedere")) {
-                    // Chiedi il tipo di postazione
-                    System.out.println("Seleziona il tipo di postazione (PRIVATO/OPENSPACE/SALA_RIUNIONI):");
-                    String tipoPostazioneStr = scanner.next();
-                    TipoPostazione tipoPostazione = TipoPostazione.valueOf(tipoPostazioneStr.toUpperCase());
-
-                    // Chiedi la città
-                    System.out.println("Inserisci la città:");
-                    String citta = scanner.next();
-
-                    // Mostra le prenotazioni filtrate per tipo di postazione e città
-                    List<Prenotazione> prenotazioniFiltrate = prenotazioniRepository.findAll().stream()
-                            .filter(p -> p.getPostazione().getTipo() == tipoPostazione && p.getPostazione().getEdificio().getCitta().equalsIgnoreCase(citta))
-                            .toList();
-
-                    System.out.println("Prenotazioni per tipo di postazione e città selezionati:");
-                    System.out.printf("%-15s %-20s %-20s %-20s%n", "Data", "Tipo Postazione", "Utente", "Città");
-                    for (Prenotazione prenotazione : prenotazioniFiltrate) {
-                        System.out.printf("%-15s %-20s %-20s %-20s%n", prenotazione.getDataPrenotazione(), prenotazione.getPostazione().getTipo(), prenotazione.getUtente().getNomeCompleto(), prenotazione.getPostazione().getEdificio().getCitta());
-                    }
+                    mostraPrenotazioniFiltrate(scanner);
                 } else {
-                    // Crea la prenotazione
                     prenotazioneUtenteService.createPrenotazioneWithScanner(utente, edificio.getId());
                 }
 
-                // Chiedi se l'utente vuole terminare o continuare
                 System.out.println("Vuoi terminare l'utilizzo del terminale o continuare? (terminare/continuare)");
-                String risposta = scanner.next();
-                if (risposta.equalsIgnoreCase("terminare")) {
+                if (scanner.next().equalsIgnoreCase("terminare")) {
                     break;
                 }
             }
 
-            // Chiedi se l'utente vuole uscire dal programma o continuare con un altro utente
             System.out.println("Vuoi uscire dal programma o continuare con un altro utente? (uscire/altro)");
-            String rispostaFinale = scanner.next();
-            if (rispostaFinale.equalsIgnoreCase("uscire")) {
+            if (scanner.next().equalsIgnoreCase("uscire")) {
                 System.out.println("Grazie per aver utilizzato il nostro servizio. Arrivederci!");
                 break;
             }
+        }
+    }
+
+    private Utente selectUtente(Scanner scanner) {
+        List<Utente> utenti = utenteRepository.findAll();
+        System.out.println("Seleziona un utente:");
+        for (int i = 0; i < utenti.size(); i++) {
+            System.out.println((i + 1) + ". " + utenti.get(i).getNomeCompleto());
+        }
+        return utenti.get(scanner.nextInt() - 1);
+    }
+
+    private Edificio selectEdificio(Scanner scanner) {
+        List<Edificio> edifici = edificioRepository.findAll();
+        System.out.println("Seleziona un edificio:");
+        for (int i = 0; i < edifici.size(); i++) {
+            System.out.println((i + 1) + ". " + edifici.get(i).getNome() + " - " + edifici.get(i).getCitta());
+        }
+        return edifici.get(scanner.nextInt() - 1);
+    }
+
+    private void mostraPrenotazioni() {
+        List<Prenotazione> prenotazioni = prenotazioniRepository.findAll();
+        System.out.println("Prenotazioni presenti nel nostro calendario:");
+        System.out.printf("%-15s %-20s %-20s %-20s%n", "Data", "Tipo Postazione", "Utente", "Città");
+        for (Prenotazione prenotazione : prenotazioni) {
+            System.out.printf("%-15s %-20s %-20s %-20s%n", prenotazione.getDataPrenotazione(), prenotazione.getPostazione().getTipo(), prenotazione.getUtente().getNomeCompleto(), prenotazione.getPostazione().getEdificio().getCitta());
+        }
+    }
+
+    private void mostraPrenotazioniFiltrate(Scanner scanner) {
+        System.out.println("Seleziona il tipo di postazione (PRIVATO/OPENSPACE/SALA_RIUNIONI):");
+        TipoPostazione tipoPostazione = TipoPostazione.valueOf(scanner.next().toUpperCase());
+
+        System.out.println("Inserisci la città:");
+        String citta = scanner.next();
+
+        List<Prenotazione> prenotazioniFiltrate = prenotazioniRepository.findAll().stream()
+                .filter(p -> p.getPostazione().getTipo() == tipoPostazione && p.getPostazione().getEdificio().getCitta().equalsIgnoreCase(citta))
+                .toList();
+
+        System.out.println("Prenotazioni per tipo di postazione e città selezionati:");
+        System.out.printf("%-15s %-20s %-20s %-20s%n", "Data", "Tipo Postazione", "Utente", "Città");
+        for (Prenotazione prenotazione : prenotazioniFiltrate) {
+            System.out.printf("%-15s %-20s %-20s %-20s%n", prenotazione.getDataPrenotazione(), prenotazione.getPostazione().getTipo(), prenotazione.getUtente().getNomeCompleto(), prenotazione.getPostazione().getEdificio().getCitta());
         }
     }
 }
